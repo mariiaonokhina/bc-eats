c431d31821ce7b9c9eb541007e850d23ef4d7359
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2024-04-04 23:52:51 -0400

    restaurant page backend with routes to get all restaurant and a specific restaurant, json file to store restaurant, frontend can render and update ui base on id given

--- js/restaurant.js
+++ js/restaurant.js
@@ -1,11 +1,66 @@
-let data;
+// HTML Elements
+const restaurantImageContainer = document.querySelectorAll(".restaurant-image");
+const iframeMap = document.querySelector(".embeded-map");
+const gMapButton = document.querySelector(".gMap-btn");
+const restaurantName = document.querySelector(".restaurant-name");
+const tagsList = document.querySelector(".tags");
+const dateList = document.querySelector(".dates");
+const descriptionP = document.querySelector(".description");
+const websiteLink = document.querySelector(".restaurant-link");
 
-fetch("./restaurant.csv")
-  .then((res) => res.text())
-  .then((csv) => {
-    let rows = csv.split("\n");
-    data = rows.map((row = row.split(",")));
-  })
-  .catch((error) => console.error(error));
+// Functionality
+document.addEventListener("DOMContentLoaded", () => {
+  const queryParams = new URLSearchParams(window.location.search);
 
-console.log(data);
+  const restaurantId = queryParams.get("id");
+
+  fetch(`http://localhost:8080/restaurants/${restaurantId}`)
+    .then((response) => response.json())
+    .then((restaurantData) => {
+      updateUI(restaurantData);
+    })
+    .catch((error) => console.error("Fetch Error:", error));
+});
+
+function updateUI(data) {
+  console.log(data);
+  // Updating Image
+  restaurantImageContainer[0].src = data["images_url"][0];
+  restaurantImageContainer[1].src = data["images_url"][1];
+
+  // Updating Iframe
+  // iframeMap.src = `${data["embeded_maps_url"]}`;
+
+  // Updating Map-Button
+  gMapButton.href = data["google_maps_url"];
+
+  // Updating Title
+  restaurantName.textContent = data["restaurant_name"];
+
+  // Updating Tags
+  data["tags"].forEach((tag) => {
+    const li = document.createElement("li");
+    li.textContent = tag;
+    tagsList.appendChild(li);
+  });
+
+  // Updating Dates
+  data["dates"].forEach((date) => {
+    const [day, ...timeArr] = date.split(" ");
+    const time = timeArr.join(" ");
+
+    const span = document.createElement("span");
+    span.textContent = day;
+
+    const li = document.createElement("li");
+    li.innerHTML = `<span>${day}</span> ${time}`;
+
+    dateList.appendChild(li);
+  });
+
+  // Updating Description:
+  descriptionP.textContent = data["description"];
+
+  // Updating Website Link
+  websiteLink.href = data["website"];
+}
