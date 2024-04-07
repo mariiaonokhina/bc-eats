ef3e50358a88eec27c5b3202fa36e1419ebaa9a7
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2024-04-07 14:04:06 -0400

    Added input to the results section of the homepage

--- js/homepage.js
+++ js/homepage.js
@@ -1,6 +1,7 @@
 // Showing the continue button only when at least one of the restrictions is checked
 const checkboxes = document.querySelectorAll('.restriction-checkbox');
 const continueBtn = document.querySelector('.continue-btn');
+const tagsSelected = document.getElementById("tags-selected");
 
 function toggleContinueButton() {
     const isAnyCheckboxChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
@@ -9,19 +10,36 @@ function toggleContinueButton() {
     else continueBtn.style.display = "none";
 }
 
+function populateTagsSelected() {
+    populateTagsSelected.innerHTML = "";    // Clear previous tags
+
+    checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            const newTag = document.createElement("span");
+            newTag.textContent = checkbox.value;
+            newTag.classList.add("tag");
+
+            tagsSelected.appendChild(newTag);
+        }
+    })
+}
+
 checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', toggleContinueButton);
+    checkbox.addEventListener("change", toggleContinueButton);
 });
 
 toggleContinueButton();
 
-document.querySelector('.continue-btn').addEventListener('click', function() {
-    // Hide the dietary restrictions container
-    document.getElementById('dietary-restrictions-container').style.display = 'none';
+document.querySelector(".continue-btn").addEventListener("click", function() {
+    // Hide the dietary restrictions container and the welcome message
+    document.getElementById("dietary-restrictions-container").style.display = "none";
+    document.getElementById("welcome-message-container").style.display = "none";
 
-    // Hide the continue button itself
-    this.style.display = 'none';
+    // Hide the continue button
+    this.style.display = "none";
 
-    // Show the hello screen
-    document.getElementById('helloScreen').style.display = 'block';
+    populateTagsSelected();
+
+    // Show the results
+    document.getElementById("results-page").style.display = "block";
 });
