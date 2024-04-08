d64016fd46a8e81596b05d0fbe052168d32cbedc
Oleksii Sudarin <asudarin22@gmail.com>
2024-04-07 22:25:00 -0400

    Improved NavBar + Favorites Page Test

--- js/favorites.js
+++ js/favorites.js
@@ -1,3 +1,29 @@
+let data = [];
+
+fetch("./server/restaurant.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => console.error("Error loading JSON file", error));
+
+const resDiv = document.getElementById("restaurantList");
+data.map((elem) => {
+  let newDiv = document.createElement("div");
+  newDiv.id = elem.id;
+  newDiv.innerHTML = `
+    <div class="flex_item_${iter%2}">
+        <button class="restaurant">
+            <p class="name">${elem.restaurant_name}</p>
+            <p class="price">${elem.price_tag}</p>
+            <p class="date">${elem.dates}</p>
+            <span class="button dangerButton smButton deleteButton" data-index="${index}">X</span>
+        </button>
+    </div>`;
+  resDiv.appendChild(newDiv);
+});
+
+/*
 let restaurants = [];
 let iter = 0;
 
@@ -7,12 +33,12 @@ function renderRestaurants() {
         const restaurantElement = document.createElement('div');
         restaurantElement.innerHTML = `
             <div for="restaurant${index}" class="flex_item_${iter%2}">
-                <div class="restaurant">
+                <button class="restaurant">
                     <p class="name">${restaurant.restaurant_name}</p>
                     <p class="price">${restaurant.price_tag}</p>
                     <p class="date">${restaurant.dates}</p>
                     <button class="button dangerButton smButton deleteButton" data-index="${index}">X</button>
-                </div>
+                </button>
             </div>
         `;
         iter++;
@@ -25,3 +51,5 @@ function renderRestaurants() {
         });
     });
 }
+
+*/
